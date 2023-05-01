8a457e4864f4b32bf6c593516469086d7927bf1f
Dillon McQuade <dillonkmcquade@gmail.com>
2023-04-30 20:44:32 -0400

    separated server from front-end

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     );
     return (
         <div>
-            {signedIn === true ? (
+            {signedIn === false ? (
                 <Box className="app">
                     <CssBaseline />
                     <Appbar />
--- src/global/Appbar.jsx
+++ src/global/Appbar.jsx
@@ -2,17 +2,17 @@ import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 function Appbar() {
-  return (
-    <AppBar
-      position="fixed"
-      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-    >
-      <Toolbar>
-        <Typography variant="h6" noWrap component="div">
-          LawnCare App
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar
+            position="fixed"
+            sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+            <Toolbar>
+                <Typography variant="h6" noWrap component="div">
+                    LawnCare App
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
 }
 export default Appbar;
